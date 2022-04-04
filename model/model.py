8844a0c0f30c75448e326dc5c7dc952c917dacec
leo_razd <razdobreev.leo@gmail.com>
2022-04-05 02:08:09 +0700

    update model

--- model/model.py
+++ model/model.py
@@ -3,16 +3,21 @@ from torchvision import models
 
 
 class Model:
-    def __init__(self, n_classes: int):
-        self.n_classes = n_classes
+    def __init__(self, n_classes: int = 3,
+                 name='resnet18',
+                 pretrained=False):
 
-    def __call__(self, name: str, pretrained: bool):
-        if name == 'resnet18':
-            resnet = models.resnet18(pretrained=pretrained)
+        self.n_classes = n_classes
+        self.name = name
+        self.pretrained = pretrained
+
+    def __call__(self):
+        if self.name == 'resnet18':
+            resnet = models.resnet18(pretrained=self.pretrained)
             resnet.fc = nn.Linear(512, self.n_classes)
             return resnet
 
-        elif name == 'efficientnet_b0':
-            efficientnet = models.efficientnet_b0(pretrained=pretrained)
+        elif self.name == 'efficientnet_b0':
+            efficientnet = models.efficientnet_b0(pretrained=self.pretrained)
             efficientnet.classifier[1] = nn.Linear(1280, self.n_classes)
             return efficientnet
--- pl_kernel/pl_kernel.py
+++ pl_kernel/pl_kernel.py
@@ -2,61 +2,57 @@ import torch
 import torch.nn as nn
 import torchmetrics
 import pytorch_lightning as pl
-from dataset import ResonancesDataset
 from model import Model
 from torch.utils.data import DataLoader
 
 
 class ResonancesDataModule(pl.LightningDataModule):
-    def __init__(self, train_img, val_img, test_img, batch_size):
+    def __init__(self, datasets: dict, config):
         super().__init__()
-        self.train_img = train_img
-        self.val_img = val_img
-        self.test_img = test_img
-        self.batch_size = batch_size
+        self.datasets = datasets
+        self.config = config
 
     def setup(self, stage=None):
-        self.train_dataset = ResonancesDataset(self.train_img, mode='train')
-        self.val_dataset = ResonancesDataset(self.val_img, mode='val')
-        self.test_dataset = ResonancesDataset(self.test_img, mode='test')
+        self.train_dataset = self.datasets['train_dataset']
+        self.val_dataset = self.datasets['val_dataset']
+        self.test_dataset = self.datasets['test_dataset']
 
     def train_dataloader(self):
         return DataLoader(
             self.train_dataset,
-            batch_size=self.batch_size,
-            shuffle=False,
-            num_workers=1
+            **config['train_dataloader']
         )
 
     def val_dataloader(self):
         return DataLoader(
             self.val_dataset,
-            batch_size=1,
-            shuffle=False,
-            num_workers=1
+            **config['val_dataloader']
         )
 
     def test_dataloader(self):
         return DataLoader(
             self.test_dataset,
-            batch_size=1,
-            shuffle=False,
-            num_workers=1
+            **config['test_dataloader']
         )
 
 
 class ResonancesClassifier(pl.LightningModule):
-    def __init__(self, n_classes: int, lr: float, name: str = 'resnet18', pretrained: bool = True):
+    def __init__(self, n_classes: int = 3,
+                 name: str = 'resnet18',
+                 pretrained: bool = False,
+                 lr=1e-4):
         super().__init__()
-        self.model = Model(n_classes)(name=name, pretrained=pretrained)
+
+        self.model = Model(n_classes)(name=name, pretrained=True)
         self.n_classes = n_classes
         self.lr = lr
+
         self.criterion = nn.CrossEntropyLoss()
 
         self.train_acc = torchmetrics.Accuracy()
         self.valid_acc = torchmetrics.Accuracy()
 
-    def forward(self, x, labels=None):
+    def forward(self, x):
         return self.model(x)
 
     def training_step(self, batch, batch_idx):
@@ -78,7 +74,19 @@ class ResonancesClassifier(pl.LightningModule):
         self.valid_acc(pred, labels)
         self.log('valid_loss', loss, on_epoch=True, prog_bar=True)
         self.log('valid_acc', self.valid_acc, on_epoch=True, prog_bar=True)
-        return loss
 
     def configure_optimizers(self):
         return torch.optim.AdamW(self.parameters(), lr=self.lr)
+
+
+class pl_kernel:
+    def __init__(self, datasets, config):
+        self.datasets = datasets
+        self.config = config
+
+    def __call__(self):
+        model = ResonancesClassifier(**self.config['model'])
+        dataset = ResonancesDataModule(self.datasets, config)
+        dataset.setup()
+
+        return model, dataset
